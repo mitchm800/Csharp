b0a63c9eac3bfbd5267d2d81417f7e0b6746adbc
Mitch M <mitch0matthews@gmail.com>
2017-05-08 18:49:30 -0400

    updated "press any key"

---
+++ CsharpProgram/CsharpProgram/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+
+        }
+    }
+}
